f4c197de6bccd0fe40dfd21ff81ad116c2f5051d
Tony Onodi <tonyonodi@gmail.com>
2021-02-14 16:09:51 +0000

    Updated implementation, things working, lots of mess.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,13 @@
 import { Optic } from "./index";
+
+// Messing about
+import { Lens, fromTraversable, Traversal } from "monocle-ts";
+import { array } from "fp-ts/lib/Array";
+import * as L from "monocle-ts/lib/Lens";
+import * as T from "monocle-ts/lib/Traversal";
+import { pipe } from "fp-ts/lib/function";
+
+// Not messing about
 type Address = {
   number: number;
   street: string;
@@ -9,6 +18,7 @@ type Person = {
   first_name: string;
   surname: string;
   address: Address;
+  pets: string[];
 };
 
 const person: Person = {
@@ -19,20 +29,72 @@ const person: Person = {
     street: "Glentrammon Road",
     city: "London",
   },
+  pets: ["Fido", "Mittens"],
 };
+
+// More messsing about
+const p = L.id<Person>();
+const ad = L.prop<Person, "address">("address");
+const pt = L.prop<Person, "pets">("pets");
+const st = L.prop<Address, "street">("street");
+
+st(ad(p)).get(person);
+
+const arrayTraversal = T.fromTraversable(array)<string>();
+// const piped = pipe(
+//   // blah
+//   p,
+//   pets,
+//   t
+// );
+
+// const x = pipe(p, ad, st);
+
+const capitalise = (s: string) => s.toUpperCase();
+
+const pets = Lens.fromProp<Person>()("pets");
+
+// var fromTraversal = function (traversal) { return new Traversal(traversal.modifyF); };
+// Lens.prototype.composeTraversal = function (ab) {
+//   return fromTraversal(pipeable_1.pipe(this, lens.asTraversal, traversal.compose(ab)));
+// };
+// const test = ad.compose(Lens.fromProp<Address>()("street"));
+
+const personToPets = new Traversal(
+  pipe(pets, pets.asTraversal, T.compose(arrayTraversal)).modifyF
+);
+
+// pipe(personToPets, L.id<string>());
+
+// const capitalised = personToPets.modify(capitalise)(person);
+
+// console.log(capitalised);
+
+const trav = fromTraversable(array)<Person>();
+
+const blah = trav
+  .composeLens(Lens.fromProp<Person>()("first_name"))
+  .set("blah")([person]);
+
+// Optic<Person[]>().__each().first_name.__setWith(capitalise)
+
+it("should be able to get a prop value", () => {
+  expect(Optic<Person>().first_name.__getFrom(person)).toEqual("Tony");
+});
 
 it("should be able to get a nested value", () => {
   expect(Optic<Person>().address.number.__getFrom(person)).toEqual(1);
 });
 
 it("should be able to set a nested value", () => {
-  expect(Optic<Person>().address.number.__setTo(10)).toEqual({
-  first_name: "Tony",
-  surname: "Onodi",
-  address: {
-    number: 10,
-    street: "Glentrammon Road",
-    city: "London",
-  },
-});
+  expect(Optic<Person>().address.number.__setTo(11)(person)).toEqual({
+    first_name: "Tony",
+    surname: "Onodi",
+    address: {
+      number: 11,
+      street: "Glentrammon Road",
+      city: "London",
+    },
+    pets: ["Fido", "Mittens"],
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import { Lens, fromTraversable } from "monocle-ts";
+import { Lens } from "monocle-ts";
+import * as L from "monocle-ts/lib/Lens";
+import * as T from "monocle-ts/lib/Traversal";
 import { array } from "fp-ts/lib/Array";
-
-const trav = fromTraversable(array)<Person>();
-
-const blah = trav
-  .composeLens(Lens.fromProp<Person>()("first_name"))
-  .set("blah")([person]);
-// Optic<Person[]>().__each().first_name.__setWith(capitalise)
+import { pipe } from "fp-ts/lib/function";
 
 type Optic<A, B> = {
   __getFrom: (a: A) => B;
   __setTo: (a: B) => (a: A) => B;
-};
+} & (B extends Array<infer I>
+  ? {
+      __arrayStuff: (a: A) => B;
+    }
+  : {});
 
 interface OpticProxyConstructor<Source extends Object, Dest extends Object> {
   new (
@@ -27,21 +27,56 @@ interface OpticProxyConstructor<Source extends Object, Dest extends Object> {
   };
 }
 
-export const Optic = <T extends object>() => {
+const arrayTraversal = T.fromTraversable(array)<string>();
+
+export const impl1 = <T extends object>() => {
   const OpticProxy = Proxy as OpticProxyConstructor<T, T>;
 
   return new OpticProxy({}, {
-    get: function <K extends keyof T>(target: any, prop: any) {
-      const oldPath = target.path || [];
-      const path = [...oldPath, prop];
+    get: function <S, K extends keyof T>(
+      target: { optic?: L.Lens<S, T> },
+      prop: K
+    ) {
+      // HACK the T in this isn't really doing much!
+      const optic = target.optic || (L.id() as L.Lens<T, T>);
+      const propLens = L.prop<T, typeof prop>(prop);
 
       if (prop === "__getFrom") {
-        return Lens.fromPath<T>()(target.path).get;
+        return optic.get;
       } else if (prop === "__setTo") {
-        return Lens.fromPath<T>()(target.path).set;
+        return optic.set;
       } else {
-        return new Proxy({ path }, this);
+        const newOptic = propLens(optic as any);
+        return new OpticProxy({ optic: newOptic }, this);
       }
     },
   } as any);
+};
+
+export const impl2 = <T extends object>() => {
+  const OpticProxy = Proxy as OpticProxyConstructor<T, T>;
+
+  return new OpticProxy({}, {
+    get: function <S, K extends keyof T>(
+      target: { optic?: L.Lens<S, T> },
+      prop: K
+    ) {
+      // HACK the T in this isn't really doing much!
+      const _lens = target.optic || ((L.id<T>() as any) as L.Lens<S, T>);
+      const lens = new Lens(_lens.get, _lens.set);
+
+      if (prop === "__getFrom") {
+        return lens.get;
+      } else if (prop === "__setTo") {
+        return lens.set;
+      } else {
+        const newOptic = lens.composeLens(Lens.fromProp<T>()(prop));
+        return new OpticProxy({ optic: newOptic }, this);
+      }
+    },
+  } as any);
+};
+
+export const Optic = <T extends object>() => {
+  return impl1<T>();
 };
